341ecd188f3c6fba1f70e82a005941516115b0de
Karim Rezeg <karim.rezeg@cbc.de>
2020-07-14 12:12:34 +0200

    extend article class

--- app/src/Entity/Article.php
+++ app/src/Entity/Article.php
@@ -24,7 +24,17 @@ class Article
     /**
      * @var string
      */
+    public $url = "";
+
+    /**
+     * @var string
+     */
     public $headline;
+
+    /**
+     * @var string
+     */
+    public $seoHeadline;
 
     /**
      * @var string
@@ -56,6 +66,22 @@ class Article
     }
 
     /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSeoHeadline(): string
+    {
+        return $this->seoHeadline;
+    }
+
+    /**
      * @param int $id
      */
     public function setId(int $id): void
@@ -77,5 +103,21 @@ class Article
     public function setKicker(string $kicker): void
     {
         $this->kicker = $kicker;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @param string $seoHeadline
+     */
+    public function setSeoHeadline(string $seoHeadline): void
+    {
+        $this->seoHeadline = $seoHeadline;
     }
 }
--- app/src/Factory/ArticleFactory.php
+++ app/src/Factory/ArticleFactory.php
@@ -9,12 +9,16 @@ class ArticleFactory
     public static function buildArticle(array $data): Article
     {
         $id = $data['id'] ?? 0;
+        $url = $data['canonicalUrl'] ?? "";
         $headline = $data['headline'] ?? "";
+        $seoHeadline = $data['teaserSEO']['headline'] ?? "";
         $kicker = $data['kicker'] ?? "";
 
         $article = new Article();
         $article->setId($id);
+        $article->setUrl($url);
         $article->setHeadline($headline);
+        $article->setSeoHeadline($seoHeadline);
         $article->setKicker($kicker);
 
         return $article;
